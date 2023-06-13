3c335a3c820e541c5baf75a6227c7cbe6e557c3f
SharpeimQ <SharpHaiyan@gmail.com>
2023-06-12 22:21:14 -0400

    Correct code and add json path

--- index.rb
+++ index.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-require 'open-uri'
-require 'nokogiri'
-require 'set'
+require 'google_drive'
 
-def count_unique_characters_from_website(url)
-  content = open(url).read
+def count_unique_chinese_characters_from_google_docs(file_id, credentials_path)
+  session = GoogleDrive::Session.from_service_account_key(credentials_path)
+  doc = session.file_by_id(file_id)
+  content = doc.export_as_string('text/plain')
 
   chinese_characters = content.scan(/[\p{Han}&&[^0-9]]/u)
   unique_characters = chinese_characters.to_set
@@ -13,6 +13,7 @@ def count_unique_characters_from_website(url)
   unique_characters.size
 end
 
-website_url = 'https://docs.google.com/document/d/1Z5OrYC2HS4boqnDMF0y_qUwa2Dkq3RIm8aEy47v-yMM/edit'
-unique_count = count_unique_characters_from_website(website_url)
-puts unique_count
+file_id = '1Z5OrYC2HS4boqnDMF0y_qUwa2Dkq3RIm8aEy47v-yMM'
+credentials_path = '/home/sharpeim/Desktop/Developer/uniq-chinese-char-count-2442e44427dd.json'
+unique_count = count_unique_chinese_characters_from_google_docs(file_id, credentials_path)
+puts "You have learned #{unique_count} unique Chinese characters!"
