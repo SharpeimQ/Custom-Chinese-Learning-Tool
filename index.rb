08ff47c7e7d6cbd0d7f83afe2ab2b5fb2efacb59
SharpeimQ <SharpHaiyan@gmail.com>
2023-06-13 17:07:26 -0400

    Refactor function into class

--- index.rb
+++ index.rb
@@ -4,39 +4,56 @@ require 'google/apis/docs_v1'
 require 'googleauth'
 require 'googleauth/stores/file_token_store'
 
-def count_unique_chinese_characters_from_google_docs(file_id, credentials_path)
-  # Set up authentication
-  token_store = Google::Auth::Stores::FileTokenStore.new(file: credentials_path)
-  authorizer = Google::Auth::ServiceAccountCredentials.make_creds(
-    json_key_io: File.open(credentials_path),
-    scope: Google::Apis::DocsV1::AUTH_DOCUMENTS_READONLY,
-    token_store: token_store
-  )
+# class containing methods to manipulate learning data
+class ChineseLearningTool
+  def initialize(file_id, credentials_path)
+    @file_id = file_id
+    @credentials_path = credentials_path
+  end
 
-  # Initialize the Google Docs API client
-  service = Google::Apis::DocsV1::DocsService.new
-  service.authorization = authorizer
+  def count_unique_char
+    authenticate
+    content = extract_document_content
+    chinese_char = extract_chinese_char(content)
+    count = chinese_char.uniq.size
+    puts "You have learned #{count} unique Chinese characters!"
+  end
 
-  # Get the content of the Google Docs file
-  document = service.get_document(file_id)
-  content = document.body.content
+  # authentication methods are kept private
+  private
 
-  chinese_characters = content.each_with_object([]) do |element, result|
-    next unless element.paragraph&.elements
+  def authenticate
+    token_store = Google::Auth::Stores::FileTokenStore.new(file: @credentials_path)
+    authorizer = Google::Auth::ServiceAccountCredentials.make_creds(
+      json_key_io: File.open(@credentials_path),
+      scope: Google::Apis::DocsV1::AUTH_DOCUMENTS_READONLY,
+      token_store: token_store
+    )
 
-    element.paragraph.elements.each do |paragraph_element|
-      next unless paragraph_element.text_run
+    # Initialize the Google Docs API client
+    @service = Google::Apis::DocsV1::DocsService.new
+    @service.authorization = authorizer
+  end
 
-      result << paragraph_element.text_run.content.scan(/[\p{Han}&&[^0-9]]/u)
-    end
-  end.flatten
+  def extract_document_content
+    @service.get_document(@file_id).body.content
+  end
 
-  unique_characters = chinese_characters.uniq
+  def extract_chinese_char(content)
+    content.each_with_object([]) do |element, result|
+      next unless element.paragraph&.elements
 
-  unique_characters.size
+      element.paragraph.elements.each do |paragraph_element|
+        next unless paragraph_element.text_run
+
+        result << paragraph_element.text_run.content.scan(/[\p{Han}&&[^0-9]]/u)
+      end
+    end.flatten
+  end
 end
 
 file_id = '1Z5OrYC2HS4boqnDMF0y_qUwa2Dkq3RIm8aEy47v-yMM'
 credentials_path = '/home/sharpeim/Desktop/Developer/uniq-chinese-char-count-2442e44427dd.json'
-unique_count = count_unique_chinese_characters_from_google_docs(file_id, credentials_path)
-puts "You have learned #{unique_count} unique Chinese characters!"
+
+chinese = ChineseLearningTool.new(file_id, credentials_path)
+chinese.count_unique_char
