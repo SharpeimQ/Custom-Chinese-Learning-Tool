b96c970e05f4cce2e8d7363cf1ac248772198a31
SharpeimQ <SharpHaiyan@gmail.com>
2023-06-16 11:35:18 -0400

    Add count command file

--- lib/count.rb
+++ lib/count.rb
@@ -5,5 +5,4 @@ require_relative 'chinese_learning_tool'
 file_id = '1Z5OrYC2HS4boqnDMF0y_qUwa2Dkq3RIm8aEy47v-yMM'
 credentials_path = '/home/sharpeim/Desktop/Developer/uniq-chinese-char-count-2442e44427dd.json'
 chinese = ChineseLearningTool.new(file_id, credentials_path)
-chinese.shuffle_unique_chars
 chinese.count_unique_chars
