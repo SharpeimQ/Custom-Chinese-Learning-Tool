b47a1c01145fd5c66659b8a8acf80127e71664af
SharpeimQ <SharpHaiyan@gmail.com>
2023-06-16 11:31:37 -0400

    Refactor code and add spec files

--- lib/chinese_learning_tool.rb
+++ lib/chinese_learning_tool.rb
@@ -59,10 +59,3 @@ class ChineseLearningTool
     end.flatten
   end
 end
-
-file_id = '1Z5OrYC2HS4boqnDMF0y_qUwa2Dkq3RIm8aEy47v-yMM'
-credentials_path = '/home/sharpeim/Desktop/Developer/uniq-chinese-char-count-2442e44427dd.json'
-
-chinese = ChineseLearningTool.new(file_id, credentials_path)
-chinese.shuffle_unique_chars
-chinese.count_unique_chars
---
+++ lib/count.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative 'chinese_learning_tool'
+
+file_id = '1Z5OrYC2HS4boqnDMF0y_qUwa2Dkq3RIm8aEy47v-yMM'
+credentials_path = '/home/sharpeim/Desktop/Developer/uniq-chinese-char-count-2442e44427dd.json'
+chinese = ChineseLearningTool.new(file_id, credentials_path)
+chinese.shuffle_unique_chars
+chinese.count_unique_chars
